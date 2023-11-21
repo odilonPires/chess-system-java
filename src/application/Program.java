308e58a7a0290a336df47a94128733d511a246ca
Odilon Pires <odilon_pires@hotmail.com>
2023-11-20 21:10:08 -0300

    Fix - better tratment to promotion

--- src/application/Program.java
+++ src/application/Program.java
@@ -40,8 +40,12 @@ public class Program {
 				}
 				
 				if (chessMatch.getPromoted() != null) {
-					System.out.print("Digita apeça que deseja promover (B/N/R/Q): ");
-					String type = sc.nextLine();
+					System.out.print("Digite a peça que deseja promover (B/N/R/Q): ");
+					String type = sc.nextLine().toUpperCase();
+					while (!type.equals("B") && !type.equals("N") && !type.equals("R") && !type.equals("Q")) {
+						System.out.print("Valor inválido! Digite a peça que deseja promover (B/N/R/Q): ");
+						type = sc.nextLine().toUpperCase();
+					}
 					chessMatch.replacePromotedPiece(type); 
 				}
 			} 
